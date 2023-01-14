03f508e41120ca16e3a94bea66608d9c2a8affa1
{Sourin <{iamsourinsarkar@gmail.com}>
2023-01-14 20:41:53 +0530

    Added print-rev-array

---
+++ Array/print-rev-array.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {1,2,3,4,5,6};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    for(int i = size - 1; i >= 0; i--) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
