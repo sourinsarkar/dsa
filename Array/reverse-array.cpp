1cb4134cceb359f70af237f12a85cdee847c0d35
Sourin <iamsourinsarkar@gmail.com>
2023-04-01 07:48:53 +0530

    Added code for reverse array

--- Array/reverse-array.cpp
+++ Array/reverse-array.cpp
@@ -2,11 +2,33 @@
 using namespace std;
 
 int main() {
-    int n, toof[n], start = 0, end = n - 1;
+    // Approach 1 -------------
 
-    cout << "Enter size: ";
-    for(int i = 0; i < n; i++)
-    {
-        cin >> i;
-    } 
+    int poo[] = {1, 2, 3, 4, 5, 6};
+
+    int size = sizeof(poo) / sizeof(poo[0]);
+
+    cout << "Before reverse: ";
+    for (int i = 0; i < size; i++)
+        cout << poo[i] << ' ';
+
+    // reverse(poo, poo + size);
+
+    // cout << "\nAfter reverse: ";
+    // for(int i = 0; i < size; i++) cout << poo[i] << ' ';
+
+    // Approach 2 ---------------
+
+    int start = 0, end = size - 1;
+    while(start <= end) {
+        swap(poo[start], poo[end]);
+        start++;
+        end--;
+    }
+
+    cout << "\nAfter reverse: ";
+    for (int i = 0; i < size; i++)
+        cout << poo[i] << ' ';
+
+    return 0;
 }
