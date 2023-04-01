d361deade8a1f8018af20d3651cc11eaa60ba9b7
Sourin <iamsourinsarkar@gmail.com>
2023-04-01 08:09:24 +0530

    Minor changes

--- Trial/Questions/EvenOdd.cpp
+++ Trial/Questions/EvenOdd.cpp
@@ -2,16 +2,16 @@
 using namespace std;
 
 bool isEven(int n) {
-    if(n & 1) return false;
-    else return true;
+    if(n & 1) return true;
+    else return false;
 }
 
 int main() {
     int n;
     cin >> n;
 
-    if(isEven(n)) cout << "Even" << '\n';
-    else cout << "Odd" << '\n';
+    if(isEven(n)) cout << "Odd" << '\n';
+    else cout << "Even" << '\n';
 
     return 0;
-}
+} 
