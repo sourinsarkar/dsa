8e3379f9671ea89025e3512df24151262118757c
Sourin <iamsourinsarkar@gmail.com>
2023-10-21 19:56:21 +0530

    Practice: Recursio - Factorial

--- Practice/rec_factorial.cpp
+++ Practice/rec_factorial.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int factorial(int n)
-{
-    if (n == 0)
+int factorial(int n) {
+    if(n == 0)
         return 1;
-    int smallFactorial = factorial(n - 1);
-    return n * smallFactorial;
+
+    int smallAns = factorial(n - 1);
+    return n * smallAns;
 }
 
-int main()
-{
+int main() {
+    cout << factorial(4) << '\n';
     cout << factorial(5) << '\n';
 
     return 0;
