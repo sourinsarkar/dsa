8553f8daa24a780ba9c8060ecbd698d0cdaddf22
Sourin <iamsourinsarkar@gmail.com>
2024-01-08 20:53:32 +0530

    Sloved: Palindrome

--- basics/count_digit/countDigits.cpp
+++ basics/count_digit/countDigits.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 using namespace std;
 
+// TC: O(d)
+// TC is proportional to the number of digits in the given input.
+
 int countDigits(int n) {
     int result = 0;
     while(n > 0) {
