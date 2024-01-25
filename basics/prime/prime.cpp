25cac1b8f9d361388099b96fc2370de5f4555f71
Sourin <iamsourinsarkar@gmail.com>
2024-01-25 12:05:39 +0530

    Optimised prime.cpp

--- basics/prime/prime.cpp
+++ basics/prime/prime.cpp
@@ -34,8 +34,8 @@ bool opt1_isPrime(int n) {
     
     if(n % 2 == 0 || n % 3 == 0) return false;
 
-    for(int i = 5; i * i <= n; i++) {
-        if(n % i == 0 || n * (i ))
+    for(int i = 5; i * i <= n; i+=6) {
+        if(n % i == 0 || n * (i + 2) == 0)
             return false;
     }
     return true;
---
+++ basics/prime/primeFactors.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+// Brut Force
+void primeFactors(int n) {
+    
+}
+
+int main()
+{
+    int n;
+    cout << "Enter: ";
+    cin >> n;
+
+    primeFactors(n);
+
+    return 0;
+}
