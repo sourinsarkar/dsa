0d5f0d4d5e5c3676c04ba5de21f4616490092e25
Sourin <iamsourinsarkar@gmail.com>
2024-07-25 12:22:10 +0530

    Basic divisor

--- basics/divisiors/divisors.cpp
+++ basics/divisiors/divisors.cpp
@@ -1,54 +1,20 @@
 #include <iostream>
-#include <vector>
-#include <cmath>
-#include <algorithm>
 
 using namespace std;
 
 void divisors(int n) {
     for(int i = 1; i <= n; i++) {
-        if (n % i == 0) {
-            cout << i << ", ";
-        }
-    }
-    cout << '\n';
-}
-
-void divisors2(int n) {
-    vector<int> ls;
-
-    // other way to write for the sqrt, cause that is a function, thus not optimized.
-    // instead use i * i <= n
-    // example: 6 * 6 <= 36 (Makes sense)
-    // case: 7 * 7 <= 36 false (Won't work)
-
-    // for(int i = 1; i <= sqrt(n); i++) {
-    for(int i = 1; i * i <= n; i++) {
         if(n % i == 0) {
-            ls.push_back(i);
-            if(i != n/i) {
-                ls.push_back(n/i);
-            }
+            cout << i << ' ';
         }
     }
-
-    sort(ls.begin(), ls.end());
-
-    for (int i = 0; i < ls.size(); i++) {
-        cout << ls[i] << ", ";
-    }
-    cout << '\n';
 }
 
 int main() {
     int n;
-
-    cout << "Enter number: ";
+    cout << "Enter a number: ";
     cin >> n;
 
-    cout << "Entered value is: " << n << '\n';
-    cout << "Answer:- " << '\n';
-
     divisors(n);
-    divisors2(n);
+    return 0;
 }
