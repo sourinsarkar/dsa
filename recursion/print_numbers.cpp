9c4ed138257d3d67bc51005bcea8df871559130b
Sourin <iamsourinsarkar@gmail.com>
2024-07-29 09:38:09 +0530

    Recursion: Reverse print numbers

--- recursion/print_numbers.cpp
+++ recursion/print_numbers.cpp
@@ -30,10 +30,31 @@ void print_numbers(int i, int n) {
     print_numbers(i++, n);
 }
 
+void print_numbers_reverse(int i, int n) {
+    if(i < 1) return;
+
+    cout << i << '\n';
+
+    print_numbers_reverse(i - 1, n);
+}
+
+void print_numbers_reverse2(int i, int n) {
+    if(n < i) return;
+
+    cout << n << '\n';
+    n--;
+
+    print_numbers_reverse2(i, n--);
+}
+
 int main() {
     print_name("Sourin");
     cout << "\nAnother code: " << '\n';
     print_name2(0, 3);
     print_numbers(1, 5);
+    cout << '\n';
+    print_numbers_reverse(5, 5);
+    cout << '\n';
+    print_numbers_reverse2(1, 5);
     return 0;
 }
