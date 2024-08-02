8c52ee95c7f46c8e0618dffc7142406b7f490bfe
Sourin <iamsourinsarkar@gmail.com>
2024-08-02 06:13:18 +0530

    Done: Recursive Factorial and Fibonacci

---
+++ recursion/factorial/code.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int factorial(int n) {
+    if(n <= 0) return 1;
+
+    return n * factorial(n - 1);
+}
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    
+
+    cout << factorial(n) << '\n';
+
+    return 0;
+}
