8097c27d3744d6dd2d2101af695cdf4f21ad5bed
Sourin <iamsourinsarkar@gmail.com>
2024-10-15 12:15:37 +0530

    Revised Selection sort

--- sorting/selectionSort/code.cpp
+++ sorting/selectionSort/code.cpp
@@ -1,47 +1,41 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
+
+void selectionSort(vector<int>& arr) {
+    for(size_t i = 0; i <= arr.size() - 2; i++) {
+        int smallest = i;
+        for(size_t j = i + 1; j <= arr.size() - 1; j++) {
+            if(arr[j] < arr[smallest]) {
+                smallest = j;
+            }
+        }
+
+        if(smallest != i) {
+            swap(arr[smallest], arr[i]);
+        }
+    }
+}
 
 int main() {
     vector<int> v;
     v.push_back(10);
-    v.push_back(50);
-    v.push_back(40);
-    v.push_back(70);
     v.push_back(20);
-    v.push_back(60);
+    v.push_back(40);
     v.push_back(30);
+    v.push_back(70);
+    v.push_back(5);
 
     for(auto i : v) {
-        cout << i << ' ';
+        cout << i << ' '; 
     }
     cout << '\n';
 
-    // sort(v.begin(), v.end());
-    // for(auto i : v) {
-    //     cout << i << ' ';
-    // }
-    // cout << '\n';
-    
-    for(size_t i = 0; i < v.size() - 1; i++) {
-        size_t smallest = i;
-        for(size_t j = i + 1; j < v.size(); j++) {
-            if(v[j] < v[smallest]) {
-                smallest = j;
-            }
-        }
-        if(smallest != i) {
-            swap(v[i], v[smallest]);
-        }
-    }
-    
+    selectionSort(v);
+
     for(auto i : v) {
-        cout << i << ' ';
+        cout << i << ' '; 
     }
     cout << '\n';
-
-
-
 }
